108048723854ef640e55981bd4f66a2d546525f6
Juan Castellanos <juancastellanosm@gmail.com>
2024-01-20 05:39:19 -0500

    Initial

---
+++ src/main/java/com/jcastellanos/demo/demojavaclientreactiveweb/DemoJavaClientReactiveWebApplication.java
@@ -0,0 +1,13 @@
+package com.jcastellanos.demo.demojavaclientreactiveweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoJavaClientReactiveWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoJavaClientReactiveWebApplication.class, args);
+	}
+
+}
