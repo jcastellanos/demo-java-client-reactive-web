e30b264853fa75705579c0edd5696d866fbdb679
Juan Castellanos <juancastellanosm@gmail.com>
2024-02-09 11:49:13 -0500

    Add http client

--- src/main/java/com/jcastellanos/demo/service/CityService.java
+++ src/main/java/com/jcastellanos/demo/service/CityService.java
@@ -1,7 +1,11 @@
 package com.jcastellanos.demo.service;
 
 import com.jcastellanos.demo.model.City;
+import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Flux;
 
 import java.util.Arrays;
@@ -9,10 +13,19 @@ import java.util.Arrays;
 @Service
 public class CityService {
 
+    private WebClient client;
+    @Value("${endpoint.service.cities.url}")
+    private String endpointCities;
+
+
+    @PostConstruct
+    public void postConstructor() {
+        this.client = WebClient.create(endpointCities);
+    }
+
     public Flux<City> getCities() {
-        return Flux.fromIterable(Arrays.asList(new City[]{new City("Bogota", "Colombia"),
-                new City("Cali", "Colombia"),
-                new City("Medellin", "Colombia"),
-                new City("Cartagena", "Colombia")}));
+        return this.client.get().uri("/cities").accept(MediaType.APPLICATION_JSON)
+                .retrieve()
+                .bodyToFlux(City.class);
     }
 }
